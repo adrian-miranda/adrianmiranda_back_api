4cc1c41d3257c1e08fb7349a0cb8a5fbe28270e0
adrian-miranda <adrian.miranda.r@gmail.com>
2024-10-17 15:58:46 -0300

    "I moved the .git folder to a mirror project because the OS was changed to Linux and version 3.11.0 had a conflict, so it was updated to 3.11.10. It now works fine along with the README and .gitignore files."

--- core/models.py
+++ core/models.py
@@ -1,8 +1,7 @@
 from django.db import models
 # NO from pygments import get_all_lexers
 
-#si
 from pygments.lexers import get_all_lexers
-# si from pygments import lexers
+# SI from pygments import lexers
 
 # Create your models here.
